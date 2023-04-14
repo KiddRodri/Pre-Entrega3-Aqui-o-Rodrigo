c631fe0beb5922a184fd10e91a03e524c8ecc63d
Rodrigo Andre <yellowcata2121@gmail.com>
2023-04-13 21:23:40 -0500

    SEGUNDO DESAFIO

--- script/script.js
+++ script/script.js
@@ -1,25 +1,61 @@
-/*CREANDO UN SIMULADOR DE NOTAS*/
+/* ESTE SIMULADOR PRIMERO NOS PERMITIRA REGISTRAR PRODUCTOS Y LUEGO UNOS VEZ REGISTRADO, VEREMOS SI EL PRODUCTO EXISTE 
+EN ESTE CASO MI SIMULADOR ES DE PRODUCTOS DE MARCAS DE ROPA (ADIDAS, NIKE, PUMA)*/
 
+/*=====================================================*/ 
 
-/*En este problema me piden que ingrese notas de acuerdo al profesor desea ingresar y sacar el promedio 
-luego de eso mostra en que condiciones esta el alumno para que los maestros se puedan esforzar mas enseñando*/
-
-let cantNotas = prompt('Ingrese cuantas notas desea registrar');
+/* DEFINIMOS LAS VARIABLES */ 
+let cantidadDatos = prompt('Cuantos productos desea ingresar');
 let i = 1;
-let suma = 0;
-let prom = 0;
+let ContentProducts = [];
 
-while(i <= cantNotas){
-    let notas = parseFloat(prompt(`Ingrese la nota ${i}`));
-    suma = suma + notas;
-    prom = suma/cantNotas;
-    i++;
+/* ENTRADA DE DATOS */
+inputDatos();
+function inputDatos(){
+    while(i <= cantidadDatos){
+        
+        let name = prompt(`Ingrese nombre del producto ${i}`);
+        let price = parseInt(prompt(`Ingrese precio del producto ${i}`));
+        let quentity = parseInt(prompt(`Ingrese cantidad del producto ${i}`));
+
+        const obtDatos = {
+            nombre: name,
+            precio: price,
+            cantidad: quentity
+        }
+    
+        i++;
+        
+        //USE UN SPREAD OPERATOR AGREGAR LOS OBJETOS AL ARRAY 
+        ContentProducts = [...ContentProducts, obtDatos];
+
+    }
+
+    console.log(ContentProducts);
+    
+    agregarDato();
+};
+
+/* USE EL PUSH PARA AGREGAR UN DATOS MAS AL ARRAY PREGUNTANDO AL USUARIO SI SE OLVIDO */
+function agregarDato(){
+    let agregar = prompt('desea agregar?')
+    if(agregar === 'si'){
+        ContentProducts.push(prompt('Ingrese nombre del producto'));
+        console.log(ContentProducts);
+    }else{
+        alert('DATOS REGISTRADOS');
+    }
+
+    validarDatos();
 }
 
-if(prom <= 10 && prom > 8){
-    alert(`El alumno obtiene un promedio de ${prom} esta en estado optimo`);
-}else if(prom <= 8 && prom > 5){
-    alert(`El alumno obtiene un promedio de ${prom} esta en estado regular`);
-}else if(prom <= 5 && prom > 0){
-    alert(`El alumno obtiene un promedio de ${prom} esta en estado desaprobado mas esfuerzo`);
+
+/* VALIDAMOS SI EL DATO EXISTE EN EL ARRAY PARA ESTO USE SOME */
+
+function validarDatos(){
+    let existe = ContentProducts.some( item =>{
+        return item.nombre === prompt('Ingrese productos que desee verificar');
+    });
+    alert(existe);
 }
+
+/* EN ESTE PROYECTO SE USO OBJETOS ARRAAY FUNCIONES Y TODO LO PEDIDO EN EL DESAFIO */
